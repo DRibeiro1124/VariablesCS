e0030f8befa34fbc3c8eabe247aadffb408aa258
Dino Ribeiro <DRibeiro1124@gmail.com>
2022-08-21 13:52:43 -0400

    "Committing the rest of my homework"

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace VariablesCS
 {
     class Program
     {
-        static void Main(string[] args)
+        static void cupOfCoffee()
         {
             //Part 1
             //Practice creating variables
@@ -17,7 +17,9 @@ namespace VariablesCS
             //Use Console.WriteLine and your variables, numberOfCupsOfCoffee, fullName, and today, to output all three on one line.
             var phrase = todayDate + " is when I found out that my dog " + fullName + " drinks " + numberOfCupsOfCoffee + " cups of coffee daily, which is good, because he is a dog!!";
             Console.WriteLine(phrase);
-
+        }
+        static string nameGreeting()
+        {
             //Part 2
 
             //Practice Getting Input From the User
@@ -27,11 +29,13 @@ namespace VariablesCS
             //Print out a greeting to the user, using their name.
             var greeting = "It is a pleasure to meet you, " + userName + "!";
             Console.WriteLine(greeting);
+            return userName;
+        }
 
+        static (string, string) favoriteNumbers()
+        {
             //Part 3
-
             //Practice Getting Different Types of Input From the User
-
             //Ask the user to input two numbers.
             //Get the numbers as strings using Console.ReadLine, store them in variables named firstNumberAsString and secondNumberAsString
             Console.Write("Can you please tell me your favorite number? ");
@@ -39,21 +43,23 @@ namespace VariablesCS
             Console.Write("Thank you! I like number " + firstNumberAsString + " too! What is your second favorite number? ");
             var secondNumberAsString = Console.ReadLine();
             Console.WriteLine("Thanks again! Your two favorite numbers are " + firstNumberAsString + " and " + secondNumberAsString);
+            return (firstNumberAsString, secondNumberAsString);
+        }
 
-            //Part 3
-            // Converting String Input Into Numbers
-            //Convert each string above to a double using double.Parse. Save the first value in a variable named firstOperand and the second value in a variable named secondOperand.
-            var firstOperand = double.Parse(firstNumberAsString);
-            var secondOperand = double.Parse(secondNumberAsString);
-
-            //Part 4
-            //Doing Math
+        static void sumOfValues(double firstOperand, double secondOperand, string userName)
+        {
             //Add the operand variables from above and save the results in a variable named sum.
             var sum = firstOperand + secondOperand;
             Console.WriteLine(userName + " if I take your two favorite numbers and add them together, the result would be " + sum);
 
+        }
+
+        static void differenceOfValues(double firstOperand, double secondOperand, string userName)
+        {
+            //Part 5
+            //Doing Math
+
             //Subtract the secondOperand variable from the firstOperand variable and save the results in a variable named difference.
-            //I did an experiment where I took 6 and 10 as my two favorite numbers and I was getting -4 so I wrote (help from the wife) a simple if/else to get the difference between the two numbers
             double difference;
             if (secondOperand > firstOperand)
             {
@@ -64,25 +70,37 @@ namespace VariablesCS
                 difference = firstOperand - secondOperand;
             }
             Console.WriteLine(userName + " also, I can take your favorite two numbers and get the difference by subtracting them and the result would be " + difference);
+        }
 
-            //Multiply the operand variables and save the results in a variable named product.
+
+        static void multiplicationValue(double firstOperand, double secondOperand, string userName)
+        {//Multiply the operand variables and save the results in a variable named product.
             var product = firstOperand * secondOperand;
             Console.WriteLine(userName + " we can also take those two favorite numbers and multiply them to get " + product);
+        }
 
+        static void quotientValue(double firstOperand, double secondOperand, string userName)
+        {
             //Divide the firstOperand by the secondOperand and save the results in a variable named quotient.
             var quotient = firstOperand / secondOperand;
             Console.WriteLine("We can also divide those same two numbers and get " + quotient);
+        }
 
+        static void remainderValue(double firstOperand, double secondOperand, string userName)
+        {
             //Find the remainder when one operand is divided by the other and save the results in a variable named remainder. See this page if you need to learn more about the modulo operator.
             var remainder = firstOperand % secondOperand;
             Console.WriteLine("The remainder of those two numbers is " + remainder);
+        }
 
+        static void specialGreeting()
+        {
             //ADVENTURE MODE
             //Using Logic
             //Add some logic to your program that prints a different, special, greeting to the user if their name happens to be Alice.
             Console.Write("What is your name? ");
             var newUser = Console.ReadLine();
-            var aliceGreeting = " Hello Alice, how about a nice cup of tea?";
+            var aliceGreeting = " Hello Alice, please be very careful with the Queen of Hearts, she is evil!";
             var newGreeting = " Hello " + newUser + " have you seen Alice? I hope she's not in Wonderland ";
             string Alice = "Alice";
             string name = " ";
@@ -94,11 +112,31 @@ namespace VariablesCS
 
             //Using DateTime
             //Use the type DateTime to represent the date variables.
-            var whatMonth = DateTime.Now
+            var whatMonth = DateTime.Now;
             Console.WriteLine(" Wow " + newUser + " I can't believe it's already " + whatMonth);
             //See the lesson on variables for some guidance on DateTime
 
         }
 
+        static void Main(string[] args)
+        {
+            cupOfCoffee();
+            string userName = nameGreeting();
+            (string firstNumberAsString, string secondNumberAsString) = favoriteNumbers();
+            //Part 4
+            // Converting String Input Into Numbers
+            //Convert each string above to a double using double.Parse. Save the first value in a variable named firstOperand and the second value in a variable named secondOperand.
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
+            sumOfValues(firstOperand, secondOperand, userName);
+            differenceOfValues(firstOperand, secondOperand, userName);
+            multiplicationValue(firstOperand, secondOperand, userName);
+            quotientValue(firstOperand, secondOperand, userName);
+            remainderValue(firstOperand, secondOperand, userName);
+            specialGreeting();
+
+        }
+
     }
+
 }
